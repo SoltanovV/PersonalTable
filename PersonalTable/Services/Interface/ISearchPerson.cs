9df6a5e3a1af020d1a61d80f3c1c9f320d376236
Vladislav <vsoltanovv@gmail.com>
2023-01-13 23:01:06 +0200

    Создание сервиса для поиска записей

--- PersonalTable/Services/Interface/ISearchPerson.cs
+++ PersonalTable/Services/Interface/ISearchPerson.cs
@@ -1,9 +1,10 @@
-﻿using PersonalTable.Model.Entity;
+﻿using PersonalTable.Model.Dto;
+using PersonalTable.Model.Entity;
 
 namespace PersonalTable.Services.Interface
 {
     public interface ISearchPerson
     {
-        public Task<Person> SearcPersonAsync();
+        public Task<IEnumerable<Person>> SearchPersonAsync(int pageNumber,PersonSearchDto person);
     }
 }
---
+++ PersonalTable/Services/SearchPerson.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalTable.Model;
+using PersonalTable.Model.Dto;
+using PersonalTable.Model.Entity;
+using PersonalTable.Services.Interface;
+
+namespace PersonalTable.Services
+{
+    public class SearchPerson : ISearchPerson
+    {
+        private readonly ApplicationContext _db;
+        private IMapper _mapper;
+
+        public SearchPerson(ApplicationContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<Person>> SearchPersonAsync(int pageNumber,PersonSearchDto personDto)
+        {
+			try
+			{
+                var skipCount = (pageNumber - 1) * 5;
+
+                var search = await _db.Person.Where(p => p.FirstName == personDto.FirstName && 
+                                                   p.LastName == personDto.LastName && 
+                                                   p.Patronymic == personDto.Patronymic &&
+                                                   p.Birthday == personDto.Birthday)
+                                              .Skip(skipCount)
+                                              .Take(5)
+                                              .ToListAsync();
+
+                return search;
+
+            }
+			catch (Exception)
+			{
+				throw;
+			}
+        }
+    }
+}
