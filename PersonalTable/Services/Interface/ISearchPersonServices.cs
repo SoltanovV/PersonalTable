2af8e566223ce6529ae3e8eb604fa4d418df3940
Vladislav <vsoltanovv@gmail.com>
2023-01-15 20:26:25 +0200

    Изменение сервисов

---
+++ PersonalTable/Services/Interface/ISearchPersonServices.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonalTable.Services.Interface;
+
+public interface ISearchPersonServices
+{
+    /// <summary>
+    /// Метод для поиска в списке
+    /// </summary>
+    /// <param name="person">Список пользователей</param>
+    /// <param name="pageNumber">Выбранная страница</param>
+    /// <returns>Список найденных записей</returns>
+    public Task<SearchPersonResponce> SearchPersonAsync(Person person, int pageNumber);
+}
---
+++ PersonalTable/Services/PersonServices.cs
@@ -0,0 +1,30 @@
+﻿using PersonalTable.Services.Interface;
+
+namespace PersonalTable.Services
+{
+    public class PersonServices : IPersonServices
+    {
+        private readonly ApplicationContext _db;
+
+        public PersonServices(ApplicationContext db)
+        {
+            _db = db;
+        }   
+
+        public async Task<Person> CreatePersonAsync(Person request)
+        {
+            try
+            {
+                var person = await _db.Person.AddAsync(request);
+                
+                await _db.SaveChangesAsync();
+
+                return person.Entity;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
